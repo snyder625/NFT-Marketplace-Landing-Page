c4b705d9887d0fdeff6eb47b63b84623a6eb1f37
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2022-10-24 23:29:23 +0500

    Commit

---
+++ src/components/SectionWrapper.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SectionWrapper = ({ title, description, showBtn, mockupImg }) => {
+  return (
+    <div>SectionWrapper</div>
+  )
+}
+
+export default SectionWrapper
