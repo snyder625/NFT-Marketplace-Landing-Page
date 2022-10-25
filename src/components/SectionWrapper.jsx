5e2855e1956360b27d0987f3d167b8fa613d99a7
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2022-10-25 22:36:08 +0500

    SectionWrapper Updated

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const SectionWrapper = ({ title, description, showBtn, mockupImg }) => {
+import styles from '../styles/Global'
+import assets from '../assets'
+
+const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
   return (
-    <div>SectionWrapper</div>
+    <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
+      <div className={`flex item-center`}></div>
+      <h1>{title}</h1>
+      <h1>{description}</h1>
+      <img src={mockupImg} alt="" />
+    </div>
   )
 }
 
