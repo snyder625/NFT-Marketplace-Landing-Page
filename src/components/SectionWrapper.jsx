5bb8f4dc79da1cea833e4b59bf3ecfd15acb31fb
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2022-10-26 21:58:54 +0500

    app download button component done

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 
 import styles from '../styles/Global'
 import assets from '../assets'
+import Button from './Button'
 
 const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
   return (
     <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
-      <div className={`flex item-center`}></div>
-      <h1>{title}</h1>
-      <h1>{description}</h1>
-      <img src={mockupImg} alt="" />
+      <div className={`flex item-center ${styles.boxClass} w-11/12 sm:w-full md:w-3/4`}>
+        <div className={`${styles.descDiv} fadeLeftMini`}>
+          <h1 className={`${styles.h1Text}`}>{title}</h1>
+          <p className={`${styles.descriptionText}`}>{description}</p>
+          {showBtn && (
+            <Button assetUrl={assets.expo} link="Deployed NFT Marketplace" />
+          )}
+        </div>
+        <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
+          <img src={mockupImg} alt="mockup" className={styles.sectionImg} />
+        </div>
+      </div>
     </div>
   )
 }
