84744860bcb46f2d238e68d5877a5d149754ecb8
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2022-10-27 22:17:39 +0500

    second section added

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ const App = () => {
   return (
     <>
       <SectionWrapper 
-        title="Your own store of Nifty NFTs. Start Selling & Growing."
+        title="Your own store of Nifty NFTs. Start Selling & Growing"
         description="Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace."
         showBtn
         mockupImg={assets.homeHero}
         banner="banner"
+      />
+      <SectionWrapper 
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
       />
     </>
   );
--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -4,11 +4,11 @@ import styles from '../styles/Global'
 import assets from '../assets'
 import Button from './Button'
 
-const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
+const SectionWrapper = ({ title, description, showBtn, mockupImg, banner, reverse }) => {
   return (
-    <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
-      <div className={`flex item-center ${styles.boxClass} w-11/12 sm:w-full md:w-3/4`}>
-        <div className={`${styles.descDiv} fadeLeftMini`}>
+    <div className={`min-h-screen ${styles.section} ${ reverse ? styles.bgWhite : styles.bgPrimary} ${banner}`}>
+      <div className={`flex item-center ${ reverse ? styles.boxReverseClass : styles.boxClass} w-11/12 sm:w-full md:w-3/4`}>
+        <div className={`${styles.descDiv} ${reverse ? "fadeRightMini" : "fadeLeftMini"} ${reverse ? styles.textRight : styles.textLeft} }`}>
           <h1 className={`${styles.h1Text}`}>{title}</h1>
           <p className={`${styles.descriptionText}`}>{description}</p>
           {showBtn && (
