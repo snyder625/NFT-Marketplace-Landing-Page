8c1682c1bf273d973ceef295052defd00bdce716
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2022-10-28 11:48:49 +0500

    features component completed

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ const App = () => {
         mockupImg={assets.homeCards}
         reverse
       />
+      <Features />
     </>
   );
 }
--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -9,14 +9,14 @@ const SectionWrapper = ({ title, description, showBtn, mockupImg, banner, revers
     <div className={`min-h-screen ${styles.section} ${ reverse ? styles.bgWhite : styles.bgPrimary} ${banner}`}>
       <div className={`flex item-center ${ reverse ? styles.boxReverseClass : styles.boxClass} w-11/12 sm:w-full md:w-3/4`}>
         <div className={`${styles.descDiv} ${reverse ? "fadeRightMini" : "fadeLeftMini"} ${reverse ? styles.textRight : styles.textLeft} }`}>
-          <h1 className={`${styles.h1Text}`}>{title}</h1>
-          <p className={`${styles.descriptionText}`}>{description}</p>
+          <h1 className={`${reverse ? styles.blackText : styles.whiteText} ${styles.h1Text}`}>{title}</h1>
+          <p className={`${reverse ? styles.blackText : styles.whiteText} ${styles.descriptionText}`}>{description}</p>
           {showBtn && (
             <Button assetUrl={assets.expo} link="Deployed NFT Marketplace" />
           )}
         </div>
         <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
-          <img src={mockupImg} alt="mockup" className={styles.sectionImg} />
+          <img src={mockupImg} alt="mockup" className={`${reverse ? "fadeLeftMini" : "fadeRightMini"} ${styles.sectionImg}`} />
         </div>
       </div>
     </div>
